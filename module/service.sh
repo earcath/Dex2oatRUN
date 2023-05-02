1ca0466b43e623977126bf1e6a60430f12a0b33e
染崎枫 <102382352+SomesakiKaede@users.noreply.github.com>
2023-05-02 15:29:04 +0800

    Add files via upload

--- module/service.sh
+++ module/service.sh
@@ -5,6 +5,7 @@ boot_operation=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^开机运行=" | cut -f2
 timing_operation=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^定时运行=" | cut -f2 -d '=')
 
 rm -rf /data/adb/Dex2oatRUN/日志.log
+touch /data/adb/Dex2oatRUN/日志.log
 
 if [ "$boot_operation" = "是" ]; then
 	echo "$(date "+%Y-%m-%d %H:%M:%S") [I] : ----------设备已开机----------" >>/data/adb/Dex2oatRUN/日志.log
