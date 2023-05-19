7c0aaa721fc90498e6a05705071e765b8daa8456
柊芸芸 <102382352+HiiragiYunyun@users.noreply.github.com>
2023-05-19 20:07:55 +0800

    Add files via upload

--- module/common.sh
+++ module/common.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-DEX2OAT_CONFIG="/data/adb/Dex2oatRUN/基础配置.prop"
-OPTIONALAPP_CONFIG="/data/adb/Dex2oatRUN/自选应用列表.prop"
 
 function read_config(){	#读取配置
+	DEX2OAT_CONFIG="/data/adb/Dex2oatRUN/基础配置.prop"
+OPTIONALAPP_CONFIG="/data/adb/Dex2oatRUN/自选应用列表.prop"
 	system_app=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^系统应用=" | cut -f2 -d '=')
 	tripartite_app=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^三方应用=" | cut -f2 -d '=')
 	optional_app=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^自选应用=" | cut -f2 -d '=')
@@ -23,103 +23,82 @@ function write_config(){
 
 function start_compare(){
 	if [[ -z $(cmp $MODDIR/old.id $MODDIR/new.id) ]]; then
-		log "D" "*没有更新系统"
+		log "没有更新系统"
 	else
 		touch $MODDIR/systemupdate
-		log "D" "*更新了系统"
+		log "更新了系统"
 	fi
 	if [[ -z $(cmp $MODDIR/oldapp-S.list $MODDIR/newapp-S.list) ]]; then
-		log "D" "*没有安装或卸载系统应用"
+		log "没有安装或卸载系统应用"
 	else
 		touch $MODDIR/appchange-S
-		log "D" "*安装或卸载了系统应用"
+		log "安装或卸载了系统应用"
 	fi
 	if [[ -z $(cmp $MODDIR/oldapp-3.list $MODDIR/newapp-3.list) ]]; then
-		log "D" "*没有安装或卸载三方应用"
+		log "没有安装或卸载三方应用"
 	else
 		touch $MODDIR/appchange-3
-		log "D" "*安装或卸载了三方应用"
+		log "安装或卸载了三方应用"
 	fi
 	if [ -e $MODDIR/moduleupdate ]; then
-		log "D" "*安装或更新了模块"
-		log "I" "*强制进行编译"
+		log "安装或更新了模块"
+		log "强制进行编译"
 	fi
 }
 
 function run_dex2oat(){	#运行
-	log "I" "*开始第$count次运行！"
 	if [[ "$system_app" != "无" && -e $MODDIR/appchange-S ]] || [[ "$system_app" != "无" && -e $MODDIR/moduleupdate ]] || [[ "$system_app" != "无" && -e $MODDIR/systemupdate ]]; then
-		log "D" "*系统应用编译模式：$system_app"
-		log "I" "*开始编译系统应用！"
+		log "系统应用编译模式：$system_app"
+		log "开始编译系统应用！"
 		source $MODDIR/mode/dexapp-S.sh
 		echo $newAPP_S >$MODDIR/oldapp-S.list
-		log "I" "*系统应用编译完毕！"
+		log "系统应用编译完毕！"
 	fi
 	if [[ "$tripartite_app" != "无" && -e $MODDIR/appchange-3 ]] || [[ "$tripartite_app" != "无" && -e $MODDIR/moduleupdate ]] || [[ "$tripartite_app" != "无" && -e $MODDIR/systemupdate ]]; then
-		log "D" "*三方应用编译模式：$tripartite_app"
-		log "I" "*开始编译三方应用！"
+		log "三方应用编译模式：$tripartite_app"
+		log "开始编译三方应用！"
 		source $MODDIR/mode/dexapp-3.sh
 		echo $newAPP_3 >$MODDIR/oldapp-3.list
-		log "I" "*三方应用编译完毕！"
+		log "三方应用编译完毕！"
 	fi
 	if [[ "$optional_app" != "无" && -e $MODDIR/appchange-3 ]] || [[ "$optional_app" != "无" && -e $MODDIR/appchange-S ]] || [[ "$optional_app" != "无" && -e $MODDIR/moduleupdate ]] || [[ "$optional_app" != "无" && -e $MODDIR/systemupdate ]]; then
-		log "D" "*自选应用编译模式：$optional_app"
-		log "I" "*开始编译自选应用！"
+		log "自选应用编译模式：$optional_app"
+		log "开始编译自选应用！"
 		source $MODDIR/mode/dexapp-O.sh
 		echo $newAPP_S >$MODDIR/oldapp-S.list
 		echo $newAPP_3 >$MODDIR/oldapp-3.list
-		log "I" "*自选应用编译完毕！"
+		log "自选应用编译完毕！"
 	fi
 	if [[ "$system_app" = "无" && "$tripartite_app" = "无" && "$optional_app" = "无" ]]; then
-		log "W" "*未选择应用！"
+		log "未选择应用！"
 	fi
-	log "I" "*运行完毕！"
+	log "运行完毕！"
 }
 
-function notification_simulation(){	#通知提醒
-	local title="${1}"
-	local text="${2}"
-	su -lp 2000 -c "cmd notification post -S messaging --conversation '${title}' --message '':'${text}' 'Tag' '$(echo $RANDOM)' "
+function rewrite_log(){
+	rm -rf /data/adb/Dex2oatRUN/日志.log
+	touch /data/adb/Dex2oatRUN/日志.log
 }
 
 function log(){	#日志
-	echo "$(date "+%Y-%m-%d %H:%M:%S") [${1}] : ${2}" >>/data/adb/Dex2oatRUN/日志.log
+	echo "$(date "+%Y-%m-%d %H:%M:%S") : ${1}" >>/data/adb/Dex2oatRUN/日志.log
 }
 
-function log_size(){
-	find /data/adb/Dex2oatRUN/日志.log -type f -size +1024k -delete
-}
-
-function counter(){	#计数
-	count=1
-	if [ -e $MODDIR/执行次数.txt ];then
-		count=$(cat $MODDIR/执行次数.txt)
-	fi
-}
-
-function counter_increase(){	#计数
-	count=$((count+1))
-	echo $count >$MODDIR/执行次数.txt
-}
-
-function delete_change(){
+function delete_mup(){
 	rm -rf $MODDIR/moduleupdate
 }
 
 function end(){
 	ends=$(grep -o '编译成功' /data/adb/Dex2oatRUN/日志.log | wc -l)
 	endf=$(grep -o '编译失败' /data/adb/Dex2oatRUN/日志.log | wc -l)
-	log "I" "*本次运行结果：成功：$ends；失败：$endf"
+	log "本次运行结果：成功：$ends；失败：$endf"
 }
 
+rewrite_log
 read_config
 write_config
 start_compare
-log_size
-counter
 run_dex2oat
-notification_simulation "dex2oat模块" "编译完成！"
-counter_increase
-delete_change
+delete_mup
 end
 
--- module/customize.sh
+++ module/customize.sh
@@ -28,26 +28,26 @@ boot_operation=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^开机运行=" | cut -f2
 timing_operation=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^定时运行=" | cut -f2 -d '=')
 run_time=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^定时执行时间=" | cut -f2 -d '=')
 Optionalapp=$(cat $OPTIONALAPP_CONFIG | grep -v '^#')
+if [ "$system_app" = "" ]; then
+	system_app="speed-profile"
+fi
+if [ "$tripartite_app" = "" ]; then
+	tripartite_app="无"
+fi
+if [ "$optional_app" = "" ]; then
+	optional_app="无"
+fi
+if [ "$boot_operation" = "" ]; then
+	boot_operation="是"
+fi
+if [ "$timing_operation" = "" ]; then
+	timing_operation="是"
+fi
+if [ "$run_time" = "" ]; then
+	run_time="00 00"
+fi
 
 #写入配置
-if [ ! -d /data/adb/modules/Dex2oatRUN ]; then
-echo "#基础配置：
-#可填：
-#无、verify、quicken、space-profile、space、speed-profile、speed、everything
-#越往后性能越好，占用空间越大。
-系统应用=speed-profile
-三方应用=无
-自选应用=无
-#运行模式：
-#填：是、否
-开机运行=是
-定时运行=是
-#定时服务的执行时间，前面是分，后面是时，例如12点： 00 12 记得空格。
-定时执行时间=00 00
-" >$DEX2OAT_CONFIG
-echo "#自选应用列表（填包名，一行一个）：
-" >$OPTIONALAPP_CONFIG
-else
 echo "#基础配置：
 #可填：
 #无、verify、quicken、space-profile、space、speed-profile、speed、everything
@@ -65,7 +65,6 @@ echo "#基础配置：
 echo "#自选列表（填包名，一行一个）：
 $Optionalapp
 " >$OPTIONALAPP_CONFIG
-fi
 
 ui_print "- 模块会根据配置自动执行dex2oat。"
 ui_print "- 配置及日志文件都在此文件夹下：/data/adb/Dex2oatBOOT"
