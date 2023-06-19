64c9dd474b3b30ca3022293b8db896e2e639fb95
林芸芸 <102382352+lin-yunyun@users.noreply.github.com>
2023-06-19 10:33:51 +0800

    Add files via upload

--- module/common.sh
+++ module/common.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-function read_config(){	#读取配置
+function read_config(){
 	DEX2OAT_CONFIG="/data/adb/Dex2oatRUN/基础配置.prop"
 OPTIONALAPP_CONFIG="/data/adb/Dex2oatRUN/自选应用列表.prop"
 	system_app=$(sed '/^#/d' "$DEX2OAT_CONFIG" | grep "^系统应用=" | cut -f2 -d '=')
@@ -28,7 +28,7 @@ function start_compare(){
 	fi
 }
 
-function run_dex2oat(){	#运行
+function run_dex2oat(){
 	if [ "$system_app" != "无" ]; then
 		log "系统应用编译模式：$system_app"
 		source $MODDIR/mode/dexapp-S.sh
@@ -51,7 +51,7 @@ function rewrite_log(){
 	touch /data/adb/Dex2oatRUN/日志.log
 }
 
-function log(){	#日志
+function log(){
 	echo "[ $(date "+%Y-%m-%d %H:%M:%S") ] ${1}" >> /data/adb/Dex2oatRUN/日志.log
 }
 
